a86c02d9eeb3d60dba251a02092fa61563b4dac8
Red <90118139+JoAzar@users.noreply.github.com>
2025-06-04 23:33:29 -0300

    Update test_unitarios.java

--- test_unitarios.java
+++ test_unitarios.java
@@ -1,7 +1,7 @@
 import org.junit.Assert; //JUnit 4
 import org.junit.Test;
 
-public busquedaTest() {
+public class busquedaTest() {
   public boolean buscar(int[] b, int x) {
     int i = 0;
     while(i < b.length && b[i] != x)
@@ -27,4 +27,27 @@ public busquedaTest() {
     boolean resultado = buscar(listaDeEnteros, buscado);
     Assert.assertTrue(resultado);
   }
+
+  @Test(expected=NullPointerException.class)
+  public void buscarEnListaVacia() {
+    int[] listaDeEnteros = {};
+    int buscado = 2;
+    buscar(listaDeEnteros, buscado);
+  }
+
+  @Test(expected=NullPointerException.class)
+  public void buscarEnListaNula() {
+    int[] listaDeEnteros = null;
+    int buscado = 1;
+    buscar(listaDeEnteros, buscado);
+  }
+  
+  @Test
+  public void buscarNegativosEnListaNegativa() {
+    int[] listaDeEnteros = {-1, -3, -5, -10};
+    int buscado = -10;
+    boolean resultado = buscar(listaDeEnteros, buscado);
+    Assert.assertEquals(-10, resultado);
+  }
+    
 }
